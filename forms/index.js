0d67b954212b7abd3affae14c95e9d1f7d4e6d31
isaac-prog <panda_mm88@yahoo.com>
2021-07-07 10:00:00 +0000

    added forms update

--- forms/index.js
+++ forms/index.js
@@ -45,6 +45,45 @@ const createProductForm = () => {
     })
 };
 
+const createProductForm = (categories) => {
+    return forms.create({
+        'name': fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            }
+        }),
+        'cost': fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            },
+            'validators':[validators.integer()]
+        }),
+        'description': fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            }
+        }),
+        'category_id': fields.string({
+            label:'Category',
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            },
+            widget: widgets.select(),
+            choices: categories
+        })
+    })
+};
+
+
+
 module.exports = { createProductForm, 
     createRegistrationForm,
     createLoginForm, 
