d1f5368412338b3b477d1886c1f5834f1054df16
isaac-prog <panda_mm88@yahoo.com>
2021-07-12 09:15:28 +0000

    added create

--- routes/products.js
+++ routes/products.js
@@ -49,6 +49,7 @@ router.post('/create', async(req,res)=>{
     //  reads in all the tags from the table and for each tag, store their id and name in an array. 
     // All the tags are then passed to the createProductForm function.
 
+    const allTags = await Tag.fetchAll().map( tag => [tag.get('id'), tag.get('name')]);
     
     const productForm = createProductForm(allCategories,allTags);
     productForm.handle(req, {
@@ -60,7 +61,7 @@ router.post('/create', async(req,res)=>{
             // 2. Save data from form into the new product instance
             // we pass all the data in the form to the product via the constructor. 
             // For this to work, the name of fields in the form must match the name of all columns in the table.
-            const product = new Product(form.data);
+            const product = new Product(productData);
 
             product.set('name', form.data.name);
             product.set('cost', form.data.cost);
@@ -73,7 +74,7 @@ router.post('/create', async(req,res)=>{
             if (tags) {
                 await product.tags().attach(tags.split(","));
             }
-            res.redirect('/products');
+            res.redirect('/');
 
         },
         'error': async (form) => {
@@ -126,13 +127,11 @@ router.get('/products/:product_id/update', async (req, res) => {
     let selectedTags = await product.related('tags').pluck('id');
     productForm.fields.tags.value= selectedTags;
 
-
     res.render('products/update', {
         'form': productForm.toHTML(bootstrapField),
         'product': product.toJSON()
     })
 })
-
 
 // this router is to push the updated information into the database
 
@@ -189,22 +188,20 @@ router.post('/products/:product_id/update', async (req, res) => {
 })
 
 // Deleting a form
-router.get('/:product_id/delete', async(req,res)=>{
+router.get('products/:product_id/delete', async(req,res)=>{
     // fetch the product that we want to delete
     const product = await Product.where({
         'id': req.params.product_id
     }).fetch({
         require: true
     });
-
     res.render('products/delete', {
         'product': product.toJSON()
     })
-
 });
 
 // process delete:
-router.post('/:product_id/delete', async(req,res)=>{
+router.post('products/:product_id/delete', async(req,res)=>{
     // fetch the product that we want to delete
     const product = await Product.where({
         'id': req.params.product_id
@@ -213,7 +210,7 @@ router.post('/:product_id/delete', async(req,res)=>{
     });
     // destroy function basically just destroys that product that we fetch above
     await product.destroy();
-    res.redirect('/products')
+    res.redirect('/')
 })
 
 module.exports = router;
